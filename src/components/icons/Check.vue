d9faa1396bdc6967fa6e98e808c09810b25b40c9
M-Facey <faceymaxal17@gmail.com>
2023-10-05 08:52:47 -0500

    feat: added check icon

---
+++ src/components/icons/Check.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="m5 12l5 5L20 7"
+    ></path>
+  </svg>
+</template>
