a97b0318f22927814ddf65dda465bbdd6e8f0a2b
M-Facey <faceymaxal17@gmail.com>
2023-10-05 15:09:09 -0500

    feat: created eye open & close icons

---
+++ src/components/icons/EyeOpen.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0"></path>
+      <path
+        d="M21 12c-2.4 4-5.4 6-9 6c-3.6 0-6.6-2-9-6c2.4-4 5.4-6 9-6c3.6 0 6.6 2 9 6"
+      ></path>
+    </g>
+  </svg>
+</template>
