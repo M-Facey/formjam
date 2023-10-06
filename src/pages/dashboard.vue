512450b0d36149837abc41ff0a31c6f90d4e27bd
M-Facey <faceymaxal17@gmail.com>
2023-10-06 08:42:42 -0500

    feat: added filter tab to dashboard page

---
+++ src/pages/dashboard.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import FilterTab from "../components/dashboard/FilterTab.vue";
+</script>
+
+<template>
+  <div class="pt-7 px-5">
+    <div class="container">
+      <FilterTab />
+    </div>
+  </div>
+</template>
