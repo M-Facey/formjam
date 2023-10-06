e03352d929baaa7ea49a17a98af133bf25900129
M-Facey <faceymaxal17@gmail.com>
2023-10-05 22:43:16 -0500

    feat: added plus & user icons

---
+++ src/components/icons/Add.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 5v14m-7-7h14"
+    ></path>
+  </svg>
+</template>
