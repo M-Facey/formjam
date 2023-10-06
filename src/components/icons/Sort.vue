f2e4eb21bc544ff488e6d6c5c048dc15043cfa07
M-Facey <faceymaxal17@gmail.com>
2023-10-06 07:10:50 -0500

    feat: added icons for filter tab

---
+++ src/components/icons/Sort.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M15 10V5c0-1.38.62-2 2-2s2 .62 2 2v5m0-3h-4m4 14h-4l4-7h-4M4 15l3 3l3-3M7 6v12"
+    ></path>
+  </svg>
+</template>
