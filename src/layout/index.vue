d0caf383401a9d87e058f3842765027f1c399e8b
M-Facey <faceymaxal17@gmail.com>
2023-10-05 22:46:21 -0500

    feat: added multiple layouts

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,16 +1,38 @@
 <script lang="ts" setup>
-import { shallowRef, watch } from "vue";
+import { shallowRef, watch, ref } from "vue";
+import type { Component } from "vue";
 import { useRoute } from "vue-router";
 
 // import layouts
 import DefaultLayout from "./default.vue";
+import AppLayout from "./app.vue";
 
 const route = useRoute();
-const layout = shallowRef<Component>(DefaultLayout);
+const layoutName = ref("Default");
+const layoutComponent = shallowRef<Component>(DefaultLayout);
+
+watch(
+  () => route.fullPath,
+  (_) => {
+    if (
+      layoutName.value === route.meta.layout ||
+      route.meta.layout === undefined
+    )
+      return;
+
+    if (route.meta.layout === "Default") {
+      layoutName.value = "Default";
+      layoutComponent.value = DefaultLayout;
+    } else {
+      layoutName.value = "App";
+      layoutComponent.value = AppLayout;
+    }
+  }
+);
 </script>
 
 <template>
-  <Component :is="layout">
+  <Component :is="layoutComponent">
     <slot />
   </Component>
 </template>
