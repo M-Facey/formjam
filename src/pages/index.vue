4a54e1e754c16bb9c8f117ceb7d45374c754bf89
M-Facey <faceymaxal17@gmail.com>
2023-10-06 11:48:55 -0500

    chore: added log in & dashboard link to home

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import XButton from "../components/inputs/Button.vue";
+</script>
+
 <template>
   <div
     class="h-screen bg-neutral-900 text-white flex flex-col items-center justify-center"
@@ -7,5 +11,15 @@
       We are currently in development, please wait as features will shortly by
       added.
     </p>
+
+    <div class="flex gap-x-4 pt-3">
+      <RouterLink to="/auth/login">
+        <XButton text="Log in" class="font-medium" />
+      </RouterLink>
+
+      <RouterLink to="/dashboard">
+        <XButton text="Dashboard" class="font-medium" />
+      </RouterLink>
+    </div>
   </div>
 </template>
