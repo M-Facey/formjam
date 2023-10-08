b46a8cfd0c627d5b3cfb5e6b77130eb2ba4e3d43
M-Facey <faceymaxal17@gmail.com>
2023-10-08 08:51:57 -0500

    feat: added form card grid component

---
+++ src/components/form/FormCardGrid.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import FormCard from "./FormCard.vue";
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-3 pt-5">
+    <FormCard
+      v-for="_ in 10"
+      name="Somethign"
+      last-edited="Oct 12, 2023"
+      created-at="Oct 01, 2023"
+    />
+  </div>
+</template>
