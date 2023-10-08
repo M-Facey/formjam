35ea4c84f081efaca6dc98fc533e9142f6eeebc8
M-Facey <faceymaxal17@gmail.com>
2023-10-08 08:39:36 -0500

    feat: added create form page

---
+++ src/pages/form/create.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import FormQuestionList from "../../components/form/FormQuestionList.vue";
+</script>
+
+<template>
+  <div class="px-5 pt-5">
+    <div class="w-full max-w-[1000px] mx-auto">
+      <p>sdasd</p>
+
+      <FormQuestionList />
+    </div>
+  </div>
+</template>
