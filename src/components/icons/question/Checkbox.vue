c847ce69a83da908d018fc33ee07ead85569beb5
M-Facey <faceymaxal17@gmail.com>
2023-10-08 07:31:52 -0500

    feat: added icons for question types

---
+++ src/components/icons/question/Checkbox.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <path d="m9 11l3 3l8-8"></path>
+      <path d="M20 12v6a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h9"></path>
+    </g>
+  </svg>
+</template>
