e6b8ade7837f25da568d6646d43e8ef3e0779fb1
M-Facey <faceymaxal17@gmail.com>
2023-10-08 08:52:42 -0500

    feat: added form card grid to dashboard

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
 import FilterTab from "../components/dashboard/FilterTab.vue";
+import FormCardGrid from "../components/form/FormCardGrid.vue";
 </script>
 
 <template>
   <div class="pt-7 px-5">
     <div class="container">
       <FilterTab />
+
+      <FormCardGrid />
     </div>
   </div>
 </template>
