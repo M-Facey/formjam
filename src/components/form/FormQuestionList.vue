5d800b33a54bce0adc4f3a9502b60c15d04fef40
M-Facey <faceymaxal17@gmail.com>
2023-10-08 08:38:21 -0500

    feat: added form question list component

---
+++ src/components/form/FormQuestionList.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import FormQuestion from "./FormQuestion.vue";
+</script>
+<template>
+  <div class="flex flex-col gap-y-3">
+    <FormQuestion v-for="_ in 5" />
+  </div>
+</template>
