cdda5e3741be28f1fdabb23d32ab168e0b2c9f33
M-Facey <faceymaxal17@gmail.com>
2023-10-09 08:27:28 -0500

    feat: added validations to text input component

--- src/components/inputs/TextInput.vue
+++ src/components/inputs/TextInput.vue
@@ -1,15 +1,23 @@
 <script lang="ts" setup>
 import { ref } from "vue";
-import { TextPropType, TextEmitType } from "../../types/inputs";
+import type { TextPropType, TextEmitType } from "../../types/inputs";
+
+import { useField } from "vee-validate";
 
 import IconEyeClose from "../icons/EyeClose.vue";
 import IconEyeOpen from "../icons/EyeOpen.vue";
 import IconSearch from "../icons/Search.vue";
+import IconAlert from "../icons/Alert.vue";
 
-withDefaults(defineProps<TextPropType>(), {
+const props = withDefaults(defineProps<TextPropType>(), {
   placeholder: "",
   isOptional: false,
   isSearchable: false,
+  modelValue: "",
+});
+
+const { value, errorMessage } = useField(() => {
+  return props.name || "";
 });
 
 defineEmits<TextEmitType>();
@@ -25,6 +33,20 @@ function toggleShowPassword() {
     <slot />
     <div class="w-[450px] relative">
       <input
+        v-if="name"
+        :id="id"
+        :type="!showPassword && type === 'password' ? type : 'text'"
+        :placeholder="placeholder"
+        v-model="value"
+        class="w-full px-4 py-3 text-black rounded-lg outline-none"
+        :class="{
+          'px-4 py-3': type !== 'password',
+          'pl-4 pr-14 py-3': type === 'password',
+          'pl-10 pr-4 py-3': isSearchable,
+        }"
+      />
+      <input
+        v-else
         :id="id"
         :type="!showPassword && type === 'password' ? type : 'text'"
         :placeholder="placeholder"
@@ -60,14 +82,15 @@ function toggleShowPassword() {
         />
       </button>
     </div>
-    <slot name="error-message" />
+    <p
+      v-if="errorMessage"
+      class="flex items-center gap-x-1 pt-1.5 text-[15px] text-red-500 tracking-wider"
+    >
+      <IconAlert class="w-6 h-6" />
+      <span class="font-medium">
+        {{ errorMessage }}
+      </span>
+    </p>
     <slot name="description" />
   </div>
 </template>
-
-<style>
-.eye-icon,
-.search-icon {
-  stroke-width: 1.5px;
-}
-</style>
