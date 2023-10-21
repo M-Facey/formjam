5ceea8fbb08d676ae62c030628f73d1e886395a0
M-Facey <faceymaxal17@gmail.com>
2023-10-20 21:20:12 -0500

    feat: added icons for navbar

--- src/components/icons/Logout.vue
+++ src/components/icons/Logout.vue
@@ -5,13 +5,17 @@
     height="1em"
     viewBox="0 0 24 24"
   >
-    <path
+    <g
       fill="none"
       stroke="currentColor"
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="2"
-      d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0-14 0m18 11l-6-6"
-    ></path>
+    >
+      <path
+        d="M14 8V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2v-2"
+      ></path>
+      <path d="M9 12h12l-3-3m0 6l3-3"></path>
+    </g>
   </svg>
 </template>
--- src/components/icons/Menu.vue
+++ src/components/icons/Menu.vue
@@ -11,7 +11,7 @@
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="2"
-      d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0-14 0m18 11l-6-6"
+      d="M4 6h16M4 12h16M4 18h16"
     ></path>
   </svg>
 </template>
