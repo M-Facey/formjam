551fba6d30ca2b3b2d08a796887eb424a7bed9d9
M-Facey <faceymaxal17@gmail.com>
2023-10-22 06:55:15 -0500

    feat: added more form types

--- src/types/form.d.ts
+++ src/types/form.d.ts
@@ -1,6 +1,79 @@
-export type FormPropType = {
-  name: string;
+// component types
+export type FormCardPropType = {
+  title: string;
+  description: string;
   lastEdited: string;
   createdAt: string;
   hasStar?: boolean;
+};
+
+export type FormCardEmitType = {
+  (e: "deleteForm"): void;
+  (e: "editForm"): void;
+};
+
+export type FormGridEmitType = {
+  (e: "trigger-event", value: FormCardEvent): void;
+};
+
+export type ErrorMessagePropType = {
+  message: string;
+};
+
+export type ErrorMessageEmitType = {
+  (e: "closeErrorMessage"): void;
+};
+
+// general types
+export type FormCardEvent = {
+  id: string;
+  name: "delete" | "rename" | "edit";
+  value: string | null;
+};
+
+export type FormError = {
+  code: number;
+  data: {
+    [key: string]: {
+      code: string;
+      message: string;
+    };
+  };
+  message: string;
+};
+
+export interface SanitizedFormType {
+  id: string;
+  created: string;
+  updated: string;
+  title: string;
+  description: string;
+  link: string;
+  preview_link: string;
+  starred: boolean;
+}
+
+export interface FormType extends SanitizedFormType {
+  collectionId: string;
+  collectionName: string;
+  user: string;
+}
+
+export type QuestionResponse = {
+  page: number;
+  perPage: number;
+  totalPages: number;
+  totalItems: number;
+  items: {
+    id: string;
+    collectionId: string;
+    collectionName: string;
+    created: string;
+    updated: string;
+    text: string;
+    type: "multiple_choice" | "checkboxes";
+    answers: string[];
+    image: string;
+    form: string;
+  }[];
 };
