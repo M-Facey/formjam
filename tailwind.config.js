28738bc0126efde588f9edc03925100ce80e654f
M-Facey <faceymaxal17@gmail.com>
2023-10-22 18:53:41 -0500

    feat: added colors to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          orange: "#F1982E",
+          pink: "#FF007A",
+          purple: "#7000FF",
+          dark: {
+            100: "#36323F",
+            500: "#1D1A2E",
+            800: "#05051E",
+          },
+        },
+      },
       fontFamily: {
         epilogue: ["Epilogue", "sans-serif"],
       },
