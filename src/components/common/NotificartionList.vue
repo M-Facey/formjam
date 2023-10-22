24861fbd3a5ff75db03032f979bb0e7b71fecad8
M-Facey <faceymaxal17@gmail.com>
2023-10-22 05:40:47 -0500

    feat: added basic notification functionality

---
+++ src/components/common/NotificartionList.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import Notification from "./Notification.vue";
+import { useNotifStore } from "../../store/notifications";
+
+const notifStore = useNotifStore();
+</script>
+
+<template>
+  <div>
+    <Notification
+      v-for="notif in notifStore.notifications"
+      :notification="notif"
+    />
+  </div>
+</template>
---
+++ src/components/common/Notification.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+defineProps<{ notification: string }>();
+</script>
+
+<template>
+  <div>
+    <h2>Notification</h2>
+    <p>{{ notification }}</p>
+  </div>
+</template>
