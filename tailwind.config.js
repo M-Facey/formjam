fe135e610f3ec13abd5397f89209a30d08ad74ce
M-Facey <faceymaxal17@gmail.com>
2023-10-22 14:14:47 -0500

    feat: added epilogue font to app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
