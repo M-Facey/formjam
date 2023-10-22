472770536eaea35f908c90650298c4552b1c3b52
M-Facey <faceymaxal17@gmail.com>
2023-10-22 06:54:26 -0500

    feat: fetched form data form pocketbase

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,6 +1,37 @@
 <script lang="ts" setup>
+import { computed, onMounted, ref } from "vue";
+import type { FormType, SanitizedFormType, FormCardEvent } from "../types/form";
+import pb from "../db/pocketBase";
+
 import FilterTab from "../components/dashboard/FilterTab.vue";
 import FormCardGrid from "../components/form/FormCardGrid.vue";
+import router from "../router";
+
+const forms = ref<FormType[]>();
+const sanitizedForms = computed<SanitizedFormType[]>(() => {
+  if (!forms.value) return [];
+
+  return forms.value.map((form) => {
+    // remove unused & sensitive data
+    const { collectionId, collectionName, user, ...rest } = form;
+    return rest;
+  });
+});
+
+async function executeEvent(event: FormCardEvent) {
+  if (event.name === "delete") {
+    await pb.collection("forms").delete(event.id);
+    forms.value = await pb
+      .collection("forms")
+      .getFullList({ sort: "-created" });
+  } else if (event.name === "edit") {
+    router.push({ name: "EditForm", params: { formId: event.id } });
+  }
+}
+
+onMounted(async () => {
+  forms.value = await pb.collection("forms").getFullList({ sort: "-created" });
+});
 </script>
 
 <template>
@@ -8,7 +39,7 @@ import FormCardGrid from "../components/form/FormCardGrid.vue";
     <div class="container">
       <FilterTab />
 
-      <FormCardGrid />
+      <FormCardGrid :forms="sanitizedForms" @trigger-event="executeEvent" />
     </div>
   </div>
 </template>
