99b0098518d12dcc8c9bca72687909dc9d7d988f
M-Facey <faceymaxal17@gmail.com>
2023-10-22 20:44:41 -0500

    chore: added pocketbase type def

--- src/db/pocketBase.ts
+++ src/db/pocketBase.ts
@@ -1,5 +1,6 @@
 import PocketBase from "pocketbase";
+import { TypedPocketBase } from "../types/pocketbase";
 const pocketBaseUrl = import.meta.env.VITE_POCKET_BASE;
 
-const pb = new PocketBase(pocketBaseUrl);
+const pb = new PocketBase(pocketBaseUrl) as TypedPocketBase;
 export default pb;
---
+++ src/types/pocketbase.d.ts
@@ -0,0 +1,51 @@
+import type { BaseAuthStore, PocketBase, RecordService } from "pocketbase";
+
+// ** Dev Note **
+// These types def for pocketbase seems to only affect
+// retreival of records from pocketbase. Methods such as
+// `getList`, `getOne`, etc.
+
+// It is completely useless for managing the data used to
+// create a record, sadly.
+
+interface BaseCollectionType {
+  id: string;
+  created: string;
+  updated: string;
+}
+
+interface Answer extends BaseCollectionType {
+  text: string;
+  question: string;
+}
+
+interface Question extends BaseCollectionType {
+  text: string;
+  type:
+    | "short-text"
+    | "paragraph"
+    | "single-choice"
+    | "checkboxes"
+    | "dropdown";
+  answers: string[];
+  form: string;
+}
+
+interface Form extends BaseCollectionType {
+  title: string;
+  description: string;
+  view: "list" | "step";
+  starred: boolean;
+  questions: string[];
+  preview_link: string;
+  link: string;
+  user: string;
+}
+
+export interface TypedPocketBase extends Pocketbase {
+  collection(idOrName: string): RecordService;
+  collection(idOrName: "answers"): RecordService<Answer>;
+  collection(idOrName: "questions"): RecordService<Question>;
+  collection(idOrName: "forms"): RecordService<Form>;
+  authStore: BaseAuthStore;
+}
