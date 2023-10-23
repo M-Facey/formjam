039622aac8c6c1dcd1618b96d246081aa2253a5d
M-Facey <faceymaxal17@gmail.com>
2023-10-23 07:09:20 -0500

    feat: check system theme on app launch

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
+import { useThemeStore } from "./store/theme";
+
 import AppLayout from "./layout/index.vue";
+
+const themeStore = useThemeStore();
+onMounted(() => {
+  themeStore.checkTheme();
+});
 </script>
 
 <template>
