19a8c0ea567da2452002b9e96f3167a0faaa7f29
M-Facey <faceymaxal17@gmail.com>
2023-10-23 07:13:31 -0500

    fix: removed type def from pocketbase

--- src/db/pocketBase.ts
+++ src/db/pocketBase.ts
@@ -1,6 +1,5 @@
 import PocketBase from "pocketbase";
-import { TypedPocketBase } from "../types/pocketbase";
 const pocketBaseUrl = import.meta.env.VITE_POCKET_BASE;
 
-const pb = new PocketBase(pocketBaseUrl) as TypedPocketBase;
+const pb = new PocketBase(pocketBaseUrl);
 export default pb;
