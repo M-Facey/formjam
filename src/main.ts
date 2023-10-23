ab8265e563415b52c4d0a4f5524af97e1041aa9a
M-Facey <faceymaxal17@gmail.com>
2023-10-23 07:07:27 -0500

    fix: used pinia plugin correctly

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 
-createApp(App).use(router).use(createPinia).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
