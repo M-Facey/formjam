5c5f72e35fad4a7698fc7636a20b993d43967caf
M-Facey <faceymaxal17@gmail.com>
2023-10-25 03:27:36 -0500

    feat: added icons for text formatting

---
+++ src/components/icons/text/Underline.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M7 5v5a5 5 0 0 0 10 0V5M5 19h14"
+    ></path>
+  </svg>
+</template>
