b0d70bb3ede3f93d064b04c6a4a4f9faf5fedf33
M-Facey <faceymaxal17@gmail.com>
2023-10-30 18:16:14 -0500

    chore: added UI tests in cypress

---
+++ cypress/specs/ui.cy.ts
@@ -0,0 +1,24 @@
+describe("UI Tests", () => {
+  it("should able to toggle theme button", () => {
+    cy.visit("/");
+
+    cy.get('[data-cy="theme_toggle"]').get("input").should("not.be.checked");
+    cy.get("html").should("not.have.class", "dark");
+
+    cy.get('[data-cy="theme_toggle"]').click();
+
+    cy.get('[data-cy="theme_toggle"]').get("input").should("be.checked");
+    cy.get("html").should("have.class", "dark");
+  });
+
+  it("should be specific content on home page", () => {
+    cy.visit("/");
+
+    cy.get('[data-cy="home_login_btn"]').should("exist");
+    cy.get('[data-cy="home_signup_btn"]').should("exist");
+    cy.get('[data-cy="theme_toggle"]').should("exist");
+
+    cy.get("h2").first().contains("Welcome to FormJAM");
+    cy.get("h2").last().contains("Revolutionize Your Data Collection");
+  });
+});
