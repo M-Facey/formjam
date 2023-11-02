6fd9b382eec3f7f6fb8177561dfec850a3d5cc9e
M-Facey <faceymaxal17@gmail.com>
2023-11-01 22:29:04 -0500

    feat: added page break icon

---
+++ src/components/icons/controls/PageBreak.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path
+        d="M14 3v4a1 1 0 0 0 1 1h4m0 10v1a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-1m-2-4h3m4.5 0h3m4.5 0h3"
+      ></path>
+      <path d="M5 10V5a2 2 0 0 1 2-2h7l5 5v2"></path>
+    </g>
+  </svg>
+</template>
