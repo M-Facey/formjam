350b9cace592c09544db0f2ea667f85bcacd8747
M-Facey <faceymaxal17@gmail.com>
2023-11-03 06:17:47 -0500

    feat: added question controls component

---
+++ src/components/form/question/QuestionControls.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import IconAdd from "@/components/icons/controls/Add.vue";
+import IconSection from "@/components/icons/controls/PageBreak.vue";
+
+defineEmits<{ (e: "add:question"): void }>();
+</script>
+
+<template>
+  <div
+    class="w-14 h-min flex flex-col gap-4 items-center bg-gray-100 py-4 rounded-lg"
+  >
+    <button @click="$emit('add:question')">
+      <IconAdd class="w-6 h-6 text-black" />
+    </button>
+
+    <button>
+      <IconSection class="w-6 h-6 text-black" />
+    </button>
+  </div>
+</template>
