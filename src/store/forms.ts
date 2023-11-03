40b9d28e35fdfd1cb1d9ad02f62ae0fcbecf3f79
M-Facey <faceymaxal17@gmail.com>
2023-11-03 06:05:09 -0500

    feat: created form store

---
+++ src/store/forms.ts
@@ -0,0 +1,45 @@
+import { defineStore } from "pinia";
+import pb from "@/db/pocketBase";
+import type { Form } from "@/types/pocketbase";
+import { useQuestionStore } from "@/store/questions";
+
+export const useFormStore = defineStore({
+  id: "forms",
+  state: () => ({
+    forms: [] as Form[],
+  }),
+  actions: {
+    async fetchForms() {
+      this.forms = await pb
+        .collection("forms")
+        .getFullList({ sort: "-created" });
+    },
+    async createForm() {
+      const questionStore = useQuestionStore();
+      const form = await pb.collection("forms").create<Form>({
+        title: "Untitled Form",
+        description: "",
+        view: "list",
+        questions: [],
+        preview_link: "",
+        link: "",
+        user: pb.authStore.model?.id,
+      });
+      const questionId = await questionStore.createQuestion(form.id);
+      this.updateForm({
+        ...form,
+        questions: [questionId],
+      });
+
+      return form.id;
+    },
+    async updateForm(form: Form) {
+      await pb.collection("forms").update(form.id, form);
+      this.fetchForms();
+    },
+    async deleteForm(formId: string) {
+      await pb.collection("forms").delete(formId);
+      this.fetchForms();
+    },
+  },
+});
