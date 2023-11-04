4316e9a48081443cbbbb031bf1b2c8bf5dd0e1a7
M-Facey <faceymaxal17@gmail.com>
2023-11-03 19:32:53 -0500

    chore: made colors adjustable

--- src/components/anim/Loader.vue
+++ src/components/anim/Loader.vue
@@ -5,9 +5,21 @@
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <circle class="spinner_S1WN" cx="4" cy="12" r="3" />
-    <circle class="spinner_S1WN spinner_Km9P" cx="12" cy="12" r="3" />
-    <circle class="spinner_S1WN spinner_JApP" cx="20" cy="12" r="3" />
+    <circle fill="currentColor" class="spinner_S1WN" cx="4" cy="12" r="3" />
+    <circle
+      fill="currentColor"
+      class="spinner_S1WN spinner_Km9P"
+      cx="12"
+      cy="12"
+      r="3"
+    />
+    <circle
+      fill="currentColor"
+      class="spinner_S1WN spinner_JApP"
+      cx="20"
+      cy="12"
+      r="3"
+    />
   </svg>
 </template>
 
