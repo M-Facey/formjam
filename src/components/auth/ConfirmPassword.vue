0cec98cdf058864ad28b4c2d382dc8fc2a01af0a
M-Facey <faceymaxal17@gmail.com>
2023-11-05 18:43:51 -0500

    feat: added success message & redirect to login

--- src/components/auth/ConfirmPassword.vue
+++ src/components/auth/ConfirmPassword.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import pb from "@/db/pocketBase";
+
 import { useForm } from "vee-validate";
 import * as yup from "yup";
 
@@ -11,12 +12,12 @@ import XTextInput from "@/components/inputs/ValidatedTextInput.vue";
 
 const { handleSubmit } = useForm({
   validationSchema: yup.object({
-    password: yup
+    newPassword: yup
       .string()
       .required("Your new password is required")
       .min(8, "Your new password must be at least 8 characters")
       .max(25, "Your new password must be at most 25 characters"),
-    confirmPassword: yup
+    confirmNewPassword: yup
       .string()
       .required("Please retype your new password")
       .oneOf(
@@ -27,16 +28,31 @@ const { handleSubmit } = useForm({
 });
 
 const route = useRoute();
+const router = useRouter();
+
+const showSuccessMessage = ref(false);
+const timeoutId = ref<NodeJS.Timeout | null>(null);
 
 const loading = ref(false);
 const errorMessage = ref("");
-const onSubmit = handleSubmit(async ({ password, confirmPassword }) => {
+const onSubmit = handleSubmit(async ({ newPassword, confirmNewPassword }) => {
+  const DURATION_IN_SECS = 2 * 1000;
   loading.value = !loading.value;
   try {
     const passwordResetToken = route.params.token as string;
     await pb
       .collection("users")
-      .confirmPasswordReset(passwordResetToken, password, confirmPassword);
+      .confirmPasswordReset(
+        passwordResetToken,
+        newPassword,
+        confirmNewPassword
+      );
+
+    showSuccessMessage.value = true;
+    timeoutId.value = setTimeout(() => {
+      showSuccessMessage.value = false;
+      router.push({ path: "/auth/login" });
+    }, DURATION_IN_SECS);
   } catch (error: any) {
     errorMessage.value = "Failed to authenticate";
   }
@@ -46,12 +62,18 @@ const onSubmit = handleSubmit(async ({ password, confirmPassword }) => {
 function clearErrorMessage() {
   errorMessage.value = "";
 }
+
+function cancelTimeout() {
+  if (timeoutId.value === null) return;
+
+  clearTimeout(timeoutId.value);
+}
 </script>
 
 <template>
   <div class="text-black dark:text-white">
     <h2 class="text-center text-4xl font-epilogue font-bold pb-6">
-      Confirm Your New Password
+      Confirm New Password
     </h2>
 
     <FormErrorMessage
@@ -60,34 +82,55 @@ function clearErrorMessage() {
       @close-error-message="clearErrorMessage"
     />
 
-    <XTextInput
-      id="new_password_input"
-      type="password"
-      name="password"
-      label="New Password"
-      data-cy="new_password_input"
-    />
-
-    <XTextInput
-      id="confirm_new_password_input"
-      type="password"
-      name="confirmPassword"
-      label="Confirm New Password"
-      data-cy="confirm_new_password_input"
-      class="mt-4"
-    />
-
-    <div class="flex items-center gap-x-4 pt-3">
-      <button
-        class="custom-btn px-4 py-1.5 text-sky-800 rounded-lg"
-        @click="onSubmit"
+    <div class="w-full max-w-[500px]">
+      <div
+        v-if="showSuccessMessage"
+        class="bg-green-500/30 mb-3 p-3 border border-green-500 rounded-lg"
       >
-        <Loader v-if="loading" class="w-6 h-6" />
-        <p v-else>Reset Password</p>
-      </button>
-      <router-link to="/auth/login" class="text-lg font-semibold underline"
-        >Return to login page</router-link
-      >
+        <p>
+          Your password was successfully reset to the new password.
+          <span class="font-bold">Please Note:</span> You will be redirected to
+          the
+          <span
+            class="bg-black/50 dark:bg-white/20 p-1 text-sm text-white font-semibold rounded-md"
+            >Login Page</span
+          >
+          shortly.
+        </p>
+      </div>
+
+      <XTextInput
+        id="new_password_input"
+        type="password"
+        name="newPassword"
+        label="New Password"
+        data-cy="new_password_input"
+      />
+
+      <XTextInput
+        id="confirm_new_password_input"
+        type="password"
+        name="confirmNewPassword"
+        label="Confirm New Password"
+        data-cy="confirm_new_password_input"
+        class="mt-4"
+      />
+
+      <div class="flex items-center gap-x-4 pt-3">
+        <button
+          class="w-[156px] custom-btn px-4 py-1.5 text-sky-800 rounded-lg"
+          @click="onSubmit"
+        >
+          <Loader v-if="loading" class="w-6 h-6 mx-auto" />
+          <p v-else>Reset Password</p>
+        </button>
+        <router-link
+          to="/auth/login"
+          class="text-lg font-semibold underline"
+          @click="cancelTimeout"
+          >Return to login page</router-link
+        >
+      </div>
     </div>
   </div>
 </template>
