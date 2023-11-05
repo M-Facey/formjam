b8a3ee814143bb6a8d932fb1c366a3a5a30fd2ad
M-Facey <faceymaxal17@gmail.com>
2023-11-05 08:01:41 -0500

    feat: added password reset page

---
+++ src/components/auth/PasswordReset.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import pb from "@/db/pocketBase";
+import { useForm } from "vee-validate";
+import * as yup from "yup";
+
+import Loader from "@/components/anim/Loader.vue";
+import FormErrorMessage from "@/components/form/FormErrorMessage.vue";
+import XTextInput from "@/components/inputs/ValidatedTextInput.vue";
+
+const { handleSubmit } = useForm({
+  validationSchema: yup.object({
+    email: yup
+      .string()
+      .email("Your email must be valid")
+      .required("Your email is required"),
+  }),
+});
+
+const loading = ref(false);
+const errorMessage = ref("");
+const onSubmit = handleSubmit(async ({ email }) => {
+  loading.value = !loading.value;
+  try {
+    await pb.collection("users").requestPasswordReset(email);
+  } catch (error: any) {
+    errorMessage.value = "Failed to authenticate";
+  }
+  loading.value = false;
+});
+</script>
+
+<template>
+  <div class="text-black dark:text-white">
+    <h2 class="text-center text-4xl font-epilogue font-bold">
+      Reset Your Password
+    </h2>
+
+    <p class="pb-5 text-sky-500 dark:text-white">
+      Fear not. We'll email you the instructions to reset your password.
+    </p>
+
+    <FormErrorMessage v-if="errorMessage" message="errorMessage" />
+    <XTextInput
+      id="reset_email_input"
+      type="text"
+      name="email"
+      label="Email Address"
+      data-cy="reset_email_input"
+    />
+
+    <div class="flex items-center gap-x-4 pt-3">
+      <button
+        class="custom-btn px-4 py-1.5 text-sky-800 rounded-lg"
+        @click="onSubmit"
+      >
+        <Loader v-if="loading" class="w-6 h-6" />
+        <p v-else>Reset Password</p>
+      </button>
+      <router-link to="/auth/login" class="text-lg font-semibold underline"
+        >Return to login page</router-link
+      >
+    </div>
+  </div>
+</template>
