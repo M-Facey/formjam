701f8200b017304baf9fea7262a3574e501ce8a5
M-Facey <faceymaxal17@gmail.com>
2023-11-05 21:52:42 -0500

    feat: added task for generating new user info

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from "cypress";
 import dotenv from "dotenv";
+import { faker } from "@faker-js/faker";
 
 dotenv.config();
 
 export default defineConfig({
   e2e: {
     baseUrl: process.env.BASE_URL,
-    specPattern: "cypress/specs/**/*.cy.{js,ts,jsx,tsx}"
+    specPattern: "cypress/specs/**/*.cy.{js,ts,jsx,tsx}",
+    setupNodeEvents(on) {
+      on("task", {
+        getFreshUser() {
+          return {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: "password",
+            confirmPassword: "password",
+          };
+        },
+      });
+    },
   },
   env: {
     API_URL: process.env.API_URL,
