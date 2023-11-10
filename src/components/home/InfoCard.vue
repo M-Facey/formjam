cac52be930a867bcde5e20d492b839b7e161279f
M-Facey <faceymaxal17@gmail.com>
2023-11-10 18:33:31 -0500

    fix: made linear gradient on info card consistent

--- src/components/home/InfoCard.vue
+++ src/components/home/InfoCard.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <div
-    class="w-full lg:w-1/3 p-4 bg-gradient-to-b from-transparent to-white dark:to-black/20 dark:md:to-primary-dark-800 border border-white dark:border-primary-dark-500 rounded-xl backdrop-blur-sm"
+    class="w-full lg:w-1/3 p-4 bg-gradient-to-b from-white/20 dark:from-transparent to-white dark:to-black/20 dark:md:to-primary-dark-800 border border-white dark:border-primary-dark-500 rounded-xl backdrop-blur-sm"
   >
     <p
       class="w-11 h-11 flex items-center justify-center bg-white/80 dark:bg-white/10 pt-0.5 text-2xl rounded-md"
