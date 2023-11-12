cb0b54fbd64f5ae031558635b3c285c4a65ebef0
M-Facey <faceymaxal17@gmail.com>
2023-11-12 12:04:39 -0500

    feat: added form card loader

---
+++ src/components/form/view/FormCardLoader.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="flex flex-col flex-stretch w-full border border-sky-100 hover:border-sky-500 rounded-md cursor-pointer overflow-hidden"
+  >
+    <div class="h-[130px] bg-sky-400"></div>
+    <div class="w-full px-2 py-3">
+      <div class="h-7 w-2/3 bg-neutral-300 animate-pulse rounded-md"></div>
+      <p class="h-5 w-full bg-neutral-300 animate-pulse mt-2 rounded-md"></p>
+    </div>
+  </div>
+</template>
