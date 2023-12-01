fb00875635969eb9e3442b1f17f94f9dfeb8b207
M-Facey <faceymaxal17@gmail.com>
2023-12-01 09:19:58 -0500

    chore: added breakpoints for tablet

--- src/utils/breakpoints.ts
+++ src/utils/breakpoints.ts
@@ -2,6 +2,8 @@ import { useBreakpoints } from "@vueuse/core";
 
 const breakpoints = useBreakpoints({
   phone: "495px",
+  tablet: "810px",
 });
 
 export const smallerOrEqual = breakpoints.smallerOrEqual('phone');
+export const lteTablet = breakpoints.smallerOrEqual('tablet');
