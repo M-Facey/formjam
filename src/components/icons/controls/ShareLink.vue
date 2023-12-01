d91cebbe671d45310cabb37d2f055b57b461214f
M-Facey <faceymaxal17@gmail.com>
2023-12-01 08:41:47 -0500

    chore: added share link icon

---
+++ src/components/icons/controls/ShareLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 14 14"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <rect
+        width="10"
+        height="10"
+        x="3.5"
+        y=".5"
+        rx="1"
+        transform="rotate(180 8.5 5.5)"
+      />
+      <path d="M10.5 13.5h-9a1 1 0 0 1-1-1v-9m7 0h3v3m0-3l-4 4" />
+    </g>
+  </svg>
+</template>
