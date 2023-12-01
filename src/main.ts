7e9233cb9871fb602ebd5dd3761f2c9d8249f6c9
M-Facey <faceymaxal17@gmail.com>
2023-11-30 21:27:26 -0500

    feat: added primevue theme styles

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import router from "@/router";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 
+import "primevue/resources/themes/lara-light-green/theme.css";
+
 createApp(App).use(router).use(createPinia()).use(PrimeVue).mount("#app");
