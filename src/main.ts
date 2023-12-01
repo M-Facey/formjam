f9f81dd895e9ac02b5dfbf2885f2173bcdc2f445
M-Facey <faceymaxal17@gmail.com>
2023-11-30 20:57:34 -0500

    feat: added primevue dependency

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import router from "@/router";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+createApp(App).use(router).use(createPinia()).use(PrimeVue).mount("#app");
