7a5c11afd52a7305b66948c77653411fbf010de6
M-Facey <faceymaxal17@gmail.com>
2023-12-01 00:43:08 -0500

    fix: made filter on dashboard responsive

--- src/components/dashboard/FilterTab.vue
+++ src/components/dashboard/FilterTab.vue
@@ -2,6 +2,8 @@
 import { ref } from "vue";
 import type { DropdownOption } from "@/types/inputs";
 
+import { smallerOrEqual } from "@/utils/breakpoints";
+
 import XDropdown from "@/components/inputs/Dropdown.vue";
 import IconGrid from "@/components/icons/layout/Grid.vue";
 import IconList from "@/components/icons/layout/List.vue";
@@ -17,15 +19,28 @@ const currentSortOption = ref<DropdownOption>(sortOptions.value[0]);
 </script>
 
 <template>
-  <div class="flex items-center">
+  <div
+    class="flex items-center"
+    :class="{
+      'flex-col items-center gap-y-3': smallerOrEqual,
+      'flex-row': !smallerOrEqual,
+    }"
+  >
     <p class="text-xl">Recent Forms</p>
 
-    <div class="flex gap-x-3 ml-auto">
-      <div class="">
+    <div
+      class="flex gap-x-3"
+      :class="{ 'w-full': smallerOrEqual, 'ml-auto': !smallerOrEqual }"
+    >
+      <div class="w-1/2">
         <p class="pb-1 text-xs uppercase tracking-widest select-none">Layout</p>
 
         <button
-          class="w-[127px] custom-btn flex items-center justify-between px-3 py-1.5 select-none cursor-pointer rounded-md"
+          class="custom-btn flex items-center justify-center px-3 py-1.5 select-none cursor-pointer rounded-md"
+          :class="{
+            'w-full': smallerOrEqual,
+            'w-[127px]': !smallerOrEqual,
+          }"
           @click="$emit('setView')"
         >
           <IconGrid v-show="view === 'Grid'" class="w-5 h-5 mr-2" />
@@ -34,8 +49,19 @@ const currentSortOption = ref<DropdownOption>(sortOptions.value[0]);
         </button>
       </div>
 
-      <div class="w-fit">
-        <p class="pb-1 text-xs uppercase tracking-widest select-none">
+      <div
+        :class="{
+          'w-1/2': smallerOrEqual,
+          'w-fit': !smallerOrEqual,
+        }"
+      >
+        <p
+          class="pb-1 text-xs uppercase tracking-widest select-none"
+          :class="{
+            'w-1/2': smallerOrEqual,
+            'w-[139px]': !smallerOrEqual,
+          }"
+        >
           Sort by
         </p>
         <XDropdown v-model="currentSortOption" :options="sortOptions" />
