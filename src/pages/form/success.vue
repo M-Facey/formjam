bf6611c0b77f480b04dcd34bc6e9373c4f0faec2
M-Facey <faceymaxal17@gmail.com>
2023-12-02 19:52:34 -0500

    chore: added link to preview/view form page

--- src/pages/form/success.vue
+++ src/pages/form/success.vue
@@ -1,13 +1,34 @@
 <script lang="ts" setup>
+import { useRoute } from "vue-router";
 import IconSuccess from "@/components/icons/misc/Success.vue";
+import { computed } from "vue";
+
+const route = useRoute();
+
+const query = computed(() => {
+  if (route.query.preview) {
+    return route.query;
+  }
+
+  return {
+    ...route.query,
+    new: "true",
+  };
+});
 </script>
 
 <template>
   <div class="flex flex-col items-center pt-16">
-    <IconSuccess class="w-12 h-12" />
+    <IconSuccess class="w-24 h-24" />
     <h1 class="py-2 text-2xl font-medium">Response Saved.</h1>
     <p>You submitted the form successfully.</p>
-    <RouterLink to="/form/id/view" class="underline">
+    <RouterLink
+      :to="{
+        path: `/form/${route.params.formId}/view`,
+        query: query,
+      }"
+      class="underline"
+    >
       Submit another response
     </RouterLink>
   </div>
