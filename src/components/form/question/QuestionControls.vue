787bff4372aacad9efd01155663a3387de879c97
M-Facey <faceymaxal17@gmail.com>
2023-12-02 20:07:39 -0500

    feat: added share dialog to question controls

--- src/components/form/question/QuestionControls.vue
+++ src/components/form/question/QuestionControls.vue
@@ -1,8 +1,41 @@
 <script lang="ts" setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
 import IconAdd from "@/components/icons/controls/Add.vue";
 import IconSection from "@/components/icons/controls/PageBreak.vue";
+import IconShare from "@/components/icons/controls/Share.vue";
+import IconCopy from "@/components/icons/controls/Copy.vue";
+
+import Dialog from "primevue/dialog";
+import { useToast } from "primevue/usetoast";
 
 defineEmits<{ (e: "add:question"): void }>();
+
+const route = useRoute();
+const toast = useToast();
+
+const visible = ref(false);
+function toggleVisible() {
+  visible.value = !visible.value;
+}
+
+const urlInput = ref<HTMLInputElement | null>(null);
+function copyText() {
+  if (urlInput.value === null) return;
+
+  urlInput.value.select();
+  urlInput.value.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(urlInput.value.value);
+
+  toast.add({
+    severity: "success",
+    summary: "Link Copied",
+    detail: "The form link was copied to clipboard",
+    life: 2000,
+  });
+}
 </script>
 
 <template>
@@ -16,5 +49,44 @@ defineEmits<{ (e: "add:question"): void }>();
     <button>
       <IconSection class="w-6 h-6 text-black" />
     </button>
+
+    <button @click="toggleVisible">
+      <IconShare class="w-6 h-6 text-black" />
+    </button>
+
+    <Dialog
+      v-model:visible="visible"
+      modal
+      header="Send Form"
+      :style="{ width: '50rem' }"
+      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+    >
+      <h2 class="text-xl">Send Form via Link</h2>
+      <div
+        class="flex items-center justify-between bg-gray-100 p-3 mt-2 rounded-lg"
+      >
+        <input
+          ref="urlInput"
+          type="text"
+          :value="`https://formjam.vercel.app/form/${route.params.formId}/view`"
+          class="w-[70%] bg-transparent py-2 border-b border-gray-100 hover:border-gray-400 focus:border-sky-500 outline-none"
+        />
+        <button class="bg-black/10 p-1.5 rounded-md" @click="copyText">
+          <IconCopy class="w-7 h-7" />
+        </button>
+      </div>
+
+      <div class="w-full flex items-center justify-end gap-x-4 mt-3">
+        <button
+          class="w-[100px] hover:bg-gray-100 active:bg-gray-200 py-2 rounded-lg"
+          @click="toggleVisible"
+        >
+          Cancel
+        </button>
+        <button class="custom-btn w-[100px] py-2 rounded-lg" @click="copyText">
+          Copy
+        </button>
+      </div>
+    </Dialog>
   </div>
 </template>
