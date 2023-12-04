18c313a4b2956253913598f0ea24b4aaf7044d42
M-Facey <faceymaxal17@gmail.com>
2023-12-03 23:16:51 -0500

    chore: added theme styles to dashboard layout

--- src/layout/dashboard.vue
+++ src/layout/dashboard.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
 import XNavbar from "@/components/dashboard/Navbar.vue";
 import XMobileNavbar from "@/components/dashboard/MobileNavbar.vue";
+import TheFooter from "@/components/common/Footer.vue";
 </script>
 
 <template>
-  <main class="min-h-[100dvh]">
+  <main class="min-h-[100dvh] dark:bg-neutral-800 flex flex-col dark:text-white">
     <XNavbar class="hidden md:block" />
     <XMobileNavbar class="block md:hidden" />
     <slot />
+    <TheFooter />
   </main>
 </template>
