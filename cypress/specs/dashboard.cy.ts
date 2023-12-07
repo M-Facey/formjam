4f9ea506defac13d0517d51f2aa4421c9b593a34
M-Facey <faceymaxal17@gmail.com>
2023-12-06 22:37:05 -0500

    chore: added delete form test & refactored no form test
    fixes #33

--- cypress/specs/dashboard.cy.ts
+++ cypress/specs/dashboard.cy.ts
@@ -4,24 +4,22 @@ describe("Dashboard tests", () => {
   beforeEach(() => {
     cy.login();
   });
-  
-  // TODO: fix this cypress test
-  // it("should only have no forms", () => {
-  //   cy.intercept(
-  //     "GET",
-  //     Cypress.env("API_URL") + "/api/collections/forms/records",
-  //     {
-  //       page: 1,
-  //       perPage: 500,
-  //       totalItems: -1,
-  //       totalPages: -1,
-  //       items: [],
-  //     }
-  //   ).as("fetchForms");
-  //   cy.wait("@fetchForms");
-  //   cy.get('[data-cy="create_form_card"]').should("exist");
-  //   cy.get('[data-cy="form_card"]').should("not.exist");
-  // });
+
+  it("should have no forms", () => {
+    cy.intercept(
+      "GET",
+      Cypress.env("API_URL") + "/api/collections/forms/records",
+      {
+        page: 1,
+        perPage: 500,
+        totalItems: -1,
+        totalPages: -1,
+        items: [],
+      }
+    ).as("fetch-forms");
+    cy.get('[data-cy="create_form_card"]').should("exist");
+    cy.get('[data-cy="form_card"]').should("not.exist");
+  });
 
   it("should able to create a new form", () => {
     cy.intercept(
@@ -30,5 +28,16 @@ describe("Dashboard tests", () => {
     ).as("create-form");
     cy.get('[data-cy="create_form_card"]').click();
     cy.wait("@create-form");
+  });
+
+  it("should able to delete a form", () => {
+    cy.intercept(
+      "DELETE",
+      Cypress.env("API_URL") + "/api/collections/forms/records/*"
+    ).as("delete-form");
+    cy.get('[data-cy="form_card_dropdown"]').first().click();
+    cy.get('[data-cy="form_card_delete_btn"]').first().click();
+    cy.get('[data-cy="dialog_confirm_delete_btn"]').click();
+    cy.wait("@delete-form");
   });
 });
