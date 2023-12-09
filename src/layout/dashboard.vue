5a79dbebe64476c46b1421993117abe8a928461d
Aundre Bryan <48803238+dre-bk201@users.noreply.github.com>
2023-12-08 21:43:16 -0500

    fix: `watcher`being triggered for both components
    Both components are still mounted regardless of setting `display: none`

--- src/layout/dashboard.vue
+++ src/layout/dashboard.vue
@@ -2,12 +2,15 @@
 import XNavbar from "@/components/dashboard/Navbar.vue";
 import XMobileNavbar from "@/components/dashboard/MobileNavbar.vue";
 import TheFooter from "@/components/common/Footer.vue";
+import { breakpoints } from "@/utils/breakpoints";
+
+const mdBP = breakpoints.greaterOrEqual("md");
 </script>
 
 <template>
   <main class="min-h-[100dvh] dark:bg-neutral-800 flex flex-col dark:text-white">
-    <XNavbar class="hidden md:block" />
-    <XMobileNavbar class="block md:hidden" />
+    <XNavbar v-if="mdBP" class="hidden md:block" />
+    <XMobileNavbar v-else class="block md:hidden" />
     <slot />
     <TheFooter />
   </main>
