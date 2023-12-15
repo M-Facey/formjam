2e7668c0c2d8e8ffc6db567f9043b25c013ac503
M-Facey <faceymaxal17@gmail.com>
2023-12-15 00:29:39 -0500

    style: added theme styles & responsiveness to form navbar

--- src/components/form/FormNavbar.vue
+++ src/components/form/FormNavbar.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import { useRoute } from "vue-router";
 
 import IconEyeOpen from "@/components/icons/input/EyeOpen.vue";
+import ThemeToggle from "@/components/common/ThemeToggle.vue";
 
 const { params } = useRoute();
 
@@ -13,27 +14,42 @@ function setSection(section: string) {
 </script>
 
 <template>
-  <header class="relative bg-neutral-100 border-b border-neutral-200 px-6 py-3">
-    <div class="container flex items-center justify-between">
+  <header class="relative bg-neutral-100 dark:bg-neutral-800">
+    <div class="container flex items-center justify-between px-6 py-3">
       <RouterLink to="/" class="flex items-center gap-x-2">
         <img src="@/assets/images/logo.png" alt="logo" class="max-h-[30px]" />
-        <p class="text-2xl text-neutral-700">
+        <p class="text-2xl text-neutral-700 dark:text-white">
           Form<span class="text-neutral-400 font-bold">JAM</span>
         </p>
       </RouterLink>
 
+      <ThemeToggle class="ml-auto mr-3" />
+      <RouterLink :to="`/form/${params.formId}/view?preview=true`">
+        <button
+          title="View Form"
+          class="custom-btn flex gap-x-2 p-2 rounded-lg"
+          data-cy="preview_btn"
+        >
+          <IconEyeOpen class="w-6 h-6 sm:mr-1" />
+          <span class="hidden sm:block"> Preview </span>
+        </button>
+      </RouterLink>
+    </div>
+    <div
+      class="flex py-3 bg-sky-100 dark:bg-neutral-700 border-y border-sky-200 dark:border-sky-500"
+    >
       <div
-        class="absolute top-0 left-1/2 -translate-x-1/2 flex items-end h-full text-neutral-600"
+        class="flex items-end h-full text-neutral-600 dark:text-white mx-auto"
       >
         <button
-          class="w-[120px] pb-3 font-medium tracking-wide"
+          class="w-[120px] font-medium tracking-wide"
           :class="{ 'text-sky-500': currentSection === 'Questions' }"
           @click="setSection('Questions')"
         >
           Questions
         </button>
         <button
-          class="w-[120px] pb-3 font-medium tracking-wide"
+          class="w-[120px] font-medium tracking-wide"
           :class="{ 'text-sky-500': currentSection === 'Responses' }"
           @click="setSection('Responses')"
         >
@@ -46,20 +62,9 @@ function setSection(section: string) {
             'translate-x-0': currentSection === 'Questions',
           }"
         >
-          <div class="w-2/3 h-[3px] bg-sky-500 rounded-t-md"></div>
+          <div class="w-2/3 h-1.5 bg-sky-500 rounded-t-md"></div>
         </div>
       </div>
-
-      <RouterLink :to="`/form/${params.formId}/view?preview=true`">
-        <button
-          title="View Form"
-          class="custom-btn flex gap-x-2 p-2 rounded-md"
-          data-cy="preview_btn"
-        >
-          <IconEyeOpen class="w-6 h-6 mr-1" />
-          Preview
-        </button>
-      </RouterLink>
     </div>
   </header>
 </template>
