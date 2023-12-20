4de71bc54b8ca7e1c65cc62eb57de8e676f506ee
M-Facey <faceymaxal17@gmail.com>
2023-12-19 20:23:03 -0500

    refactor: moved theme styles from edit page to form layout

--- src/layout/form.vue
+++ src/layout/form.vue
@@ -3,7 +3,7 @@ import FormNavbar from "@/components/form/FormNavbar.vue";
 </script>
 
 <template>
-  <main class="min-h-[100dvh]">
+  <main class="min-h-[100dvh] bg-white dark:bg-neutral-800">
     <FormNavbar />
     <slot />
   </main>
