93b44dec9cb67d484b4aac80acbc71c1e59ea4f5
M-Facey <faceymaxal17@gmail.com>
2023-12-19 19:33:22 -0500

    refactor: removed unnecessary routing code

--- src/pages/form/success.vue
+++ src/pages/form/success.vue
@@ -1,20 +1,8 @@
 <script lang="ts" setup>
-import { useRoute } from "vue-router";
+import { useRouter } from "vue-router";
 import IconSuccess from "@/components/icons/misc/Success.vue";
-import { computed } from "vue";
 
-const route = useRoute();
-
-const query = computed(() => {
-  if (route.query.preview) {
-    return route.query;
-  }
-
-  return {
-    ...route.query,
-    new: "true",
-  };
-});
+const router = useRouter();
 </script>
 
 <template>
@@ -22,14 +10,8 @@ const query = computed(() => {
     <IconSuccess class="w-24 h-24" />
     <h1 class="py-2 text-2xl font-medium">Response Saved.</h1>
     <p>You submitted the form successfully.</p>
-    <RouterLink
-      :to="{
-        path: `/form/${route.params.formId}/view`,
-        query: query,
-      }"
-      class="underline"
-    >
+    <button @click="router.go(-1)" class="underline">
       Submit another response
-    </RouterLink>
+    </button>
   </div>
 </template>
